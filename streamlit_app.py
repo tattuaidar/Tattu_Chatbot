f4d479f892b3a4d4df27568b1b7332db92a5457f
tattuaidar <tattuaidar@gmail.com>
2024-12-23 20:31:45 +0000

    'origin/main'

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,38 +2,43 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Chatbot created by Tattu Aidar")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot that helps you to learn English grammar as a non-eglish speaker."
+    "To use this app, you need to provide an OpenAI API key, which you can find in email. "
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.text_input("OpenAI API Key", type="password")
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
-
     # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
     if "messages" not in st.session_state:
-        st.session_state.messages = []
+        # Initialize the session with a system message to provide instructions.
+        st.session_state.messages = [
+            {
+                "role": "system",
+                "content": (
+                    "You are a helpful assistant that specializes in teaching English language grammar as a non-eglish speaker "
+                    "You provide clear, friendly, "
+                    "and accurate responses to user queries about grammaticle rules, writing sentences, "
+                    "and other related topics."
+                ),
+            }
+        ]
 
     # Display the existing chat messages via `st.chat_message`.
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
+    # Create a chat input field to allow the user to enter a message.
     if prompt := st.chat_input("What is up?"):
-
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
@@ -41,11 +46,8 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
+            model="gpt-4o",
+            messages=st.session_state.messages,
             stream=True,
         )
 
